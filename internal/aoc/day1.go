484875f9d6081832576526b86e43caa4d50434c5
olipratt <olipratt@users.noreply.github.com>
2023-12-02 17:04:47 +0000

    Second problem of day 1

--- internal/aoc/day1.go
+++ internal/aoc/day1.go
@@ -1,20 +1,23 @@
 package aoc
 
 import (
+	"errors"
 	"log"
 	"strings"
 )
 
+var errNoDigit = errors.New("no digit error")
+
 // Calculate the numbers created by finding the first and last digits and
 // concatenating them in each line of the given string.
-func Day1(input string) []int {
+func Day1(input string, includeWords bool) []int {
 	lines := strings.Split(input, "\n")
 
 	result := []int{}
 
 	for _, l := range lines {
-		firstDigit := firstDigitInString(l)
-		lastDigit := firstDigitInString(reverseString(l))
+		firstDigit := firstDigitInString(l, includeWords)
+		lastDigit := lastDigitInString(l, includeWords)
 
 		result = append(result, firstDigit*10+lastDigit)
 	}
@@ -22,10 +25,11 @@ func Day1(input string) []int {
 	return result
 }
 
-func firstDigitInString(input string) int {
-	for _, c := range input {
-		if strings.ContainsRune("0123456789", c) {
-			return int(c) - int('0')
+func firstDigitInString(input string, includeWords bool) int {
+	for i := 0; i < len(input); i++ {
+		digit, err := digitAtIndexInString(input, i, includeWords)
+		if err == nil {
+			return digit
 		}
 	}
 
@@ -33,6 +37,43 @@ func firstDigitInString(input string) int {
 	return -1
 }
 
+func lastDigitInString(input string, includeWords bool) int {
+	for i := len(input) - 1; i >= 0; i-- {
+		digit, err := digitAtIndexInString(input, i, includeWords)
+		if err == nil {
+			return digit
+		}
+	}
+
+	log.Fatalf("No int in string %v", input)
+	return -1
+}
+
+func digitAtIndexInString(input string, index int, includeWords bool) (int, error) {
+	// Strings for all digits with indices equal to their value.
+	digitStrings := []string{"zero", "one", "two", "three", "four", "five",
+		"six", "seven", "eight", "nine"}
+
+	char := rune(input[index])
+
+	if strings.ContainsRune("0123456789", char) {
+		return int(char) - int('0'), nil
+	}
+	if includeWords {
+		// Get the substring starting at this point and see if any of the
+		// word for digits prefix that substring.
+		substring := input[index:]
+		for digit, digitWord := range digitStrings {
+			if strings.HasPrefix(substring, digitWord) {
+				return digit, nil
+			}
+		}
+	}
+
+	return 0, errNoDigit
+}
+
+//nolint:unused // May be useful later.
 func reverseString(input string) string {
 	length := len(input)
 	result := make([]rune, length)
--- internal/aoc/day1_test.go
+++ internal/aoc/day1_test.go
@@ -46,14 +46,36 @@ func TestDay1(t *testing.T) {
 	a1b2c3d4e5f
 	treb7uchet`
 
-	values := aoc.Day1(input)
+	values := aoc.Day1(input, false)
 	assert.Equal(t, []int{12, 38, 15, 77}, values)
 
 	input = readFile(t, "input/day1.txt")
-	values = aoc.Day1(input)
+	values = aoc.Day1(input, false)
 	total := 0
 	for _, value := range values {
 		total += value
 	}
 	assert.Equal(t, 54697, total)
+}
+func TestDay1b(t *testing.T) {
+	t.Parallel()
+
+	input := `two1nine
+	eightwothree
+	abcone2threexyz
+	xtwone3four
+	4nineeightseven2
+	zoneight234
+	7pqrstsixteen`
+
+	values := aoc.Day1(input, true)
+	assert.Equal(t, []int{29, 83, 13, 24, 42, 14, 76}, values)
+
+	input = readFile(t, "input/day1.txt")
+	values = aoc.Day1(input, true)
+	total := 0
+	for _, value := range values {
+		total += value
+	}
+	assert.Equal(t, 54885, total)
 }
